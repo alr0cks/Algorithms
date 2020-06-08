5f0a67300724f469bbb24c02216b1181eba66730
Alok Bhawankar <alok.bhawankar@gmail.com>
2020-06-08 18:29:58 +0000

    fabonacci updated

--- Algorithmic ToolBox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Algorithmic ToolBox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,18 @@
 # Uses python3
+
+
 def calc_fib(n):
-    if (n <= 1):
+    if n <= 1:
         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    previous = 0
+    current = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+
+    return current
+
 
 n = int(input())
 print(calc_fib(n))
