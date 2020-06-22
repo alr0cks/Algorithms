bdc65d88654ad307f3450072eeab392b67aa0a4c
Alok Bhawankar <alok.bhawankar@gmail.com>
2020-06-22 18:44:47 +0000

    Update gcd.py

--- Algorithmic ToolBox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ Algorithmic ToolBox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,5 +1,7 @@
 # Uses python3
 import sys
+import unittest
+
 
 def gcd_naive(a, b):
     current_gcd = 1
@@ -10,7 +12,44 @@ def gcd_naive(a, b):
 
     return current_gcd
 
-if __name__ == "__main__":
+
+def gcd_euclid(a, b):
+    dividend = a if (a >= b) else b
+    divisor = a if (a <= b) else b
+
+    while divisor != 0:
+        remainder = dividend % divisor
+        dividend = divisor
+        divisor = remainder
+
+    return dividend
+
+
+class MyTest(unittest.TestCase):
+    def test_naive(self):
+        self.assertEqual(gcd_naive(18, 35), 1)
+        self.assertEqual(gcd_naive(28851538, 1183019), 17657)
+
+    def test_fast(self):
+        self.assertEqual(gcd_euclid(18, 35), 1)
+        self.assertEqual(gcd_euclid(28851538, 1183019), 17657)
+
+
+if __name__ == '__main__':
+    # unittest.main()
+
+    # while(True):
+    # 	a = random.randint(1, 10000)
+    # 	b = random.randint(2, 10000)
+
+    # 	res1 = gcd_naive(a, b)
+    # 	res2 = gcd_euclid(a, b)
+    # 	if (res1 != res2):
+    # 		print('Wrond answer: {} {}'.format(res1, res2))
+    # 		break
+    # 	else:
+    # 		print('OK: {} {}'.format(res1, res2))
+
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    print(gcd_euclid(a, b))
